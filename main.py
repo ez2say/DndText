2509f86c56d1d5768087dd2fd095c7bfa6b98837
ez2say <llityka1@yandex.ru>
2024-04-29 05:37:44 +0300

    Second commit(yep after thrd)

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import input_data
 from Dungeon import fork
 from Dungeon.Dungeon_top import forest
 from Dungeon.Dungeon_top import enemy_top
+from Dungeon.Dungeon_top import swamp
 
 
 
@@ -28,18 +29,23 @@ def run():
 
 
 def new_game():
+    dict_forest = forest.forest_dict
     hero = fork.hero_choise()
     enemy = enemy_top.enemy_list
     fork.history()
     choise = fork.first_fork()
     if choise == 1:
-        forest.forest_info()
-        forest.first_fight(hero, enemy)
+        forest.forest_info(dict_forest)
+        forest.first_fight(dict_forest,hero, enemy)
         go_to = forest.second_fork()
         if go_to == 1:
-            forest.zemlyanka(hero)
+            forest.zemlyanka(dict_forest,hero)
             forest.after_zemlyanka()
-            forest.deep_forest()
+            way = forest.deep_forest()
+            if way == 1:
+                swamp.swamp()
+            else:
+                forest.third_fork()
         else:
             forest.deep_forest()
 
