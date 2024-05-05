c18faf6fa4d646623e997a010226ca6dd5b9a5d6
ez2say <llityka1@yandex.ru>
2024-05-06 02:11:20 +0300

    Я сбился со счета какой коммит

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from Dungeon import fork
 from Dungeon.Dungeon_top import forest
 from Dungeon.Dungeon_top import enemy_top
 from Dungeon.Dungeon_top import swamp
+from Dungeon.Dungeon_top import location
 
 
 
@@ -28,27 +29,72 @@ def run():
             print(ex)
 
 
-def new_game():
-    dict_forest = forest.forest_dict
-    hero = fork.hero_choise()
-    enemy = enemy_top.enemy_list
-    fork.history()
-    choise = fork.first_fork()
-    if choise == 1:
-        forest.forest_info(dict_forest)
-        forest.first_fight(dict_forest,hero, enemy)
-        go_to = forest.second_fork()
-        if go_to == 1:
-            forest.zemlyanka(dict_forest,hero)
-            forest.after_zemlyanka()
-            way = forest.deep_forest()
-            if way == 1:
-                swamp.swamp()
-            else:
-                forest.third_fork()
-        else:
-            forest.deep_forest()
 
+def new_game():
+    char = fork.create_char()
+    new_hero = fork.set_name(char)
+    complete_hero = fork.hero_choise(new_hero)
+    print(complete_hero)
+    if complete_hero :
+        chs = fork.first_fork()
+        if chs == 1:
+            enemy_dict = enemy_top.enemy_list
+            dict_forest = location.forest_dict
+            forest.forest_info(dict_forest)
+            forest.fight_text(dict_forest)
+            forest.first_fight(complete_hero,enemy_dict)
+        elif chs == 2:
+            pass
+        else:
+            pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    #dict_forest = location.forest_dict
+    #hero = fork.hero_choise()
+    #enemy = enemy_top.enemy_list
+    #choise = fork.first_fork()
+    #if choise == 1:
+        #forest.forest_info(dict_forest)
+        #forest.first_fight(dict_forest,hero, enemy)
+        #go_to = forest.second_fork()
+        #if go_to == 1:
+           # forest.zemlyanka(dict_forest,hero)
+            #forest.after_zemlyanka()
+            #way = forest.deep_forest()
+           # if way == 1:
+           #     swamp.swamp()
+          #  else:
+            #    forest.third_fork()
+       # else:
+         #   forest.deep_forest()
+#
 
 
 
